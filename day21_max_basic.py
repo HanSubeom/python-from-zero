e5318abcae71a5e4a229c08985a77712a5e86de9
HanSubeom <rrenaissances@naver.com>
2026-03-01 13:08:11 +0900

    Day21: reinforced max logic without built-in functions

---
+++ day21_max_basic.py
@@ -0,0 +1,13 @@
+a = 3
+b = 7
+c = 5
+
+max_value = a
+
+if b > max_value:
+	max_value = b
+
+if c > max_value:
+	max_value = c
+
+print("Max:", max_value)
---
+++ day21_max_until_zero.py
@@ -0,0 +1,18 @@
+max_value = None
+
+while True:
+    number = int(input("Enter a number (0 to quit): "))
+
+    if number == 0:
+        break
+
+    if max_value is None:
+        max_value = number
+    elif number > max_value:
+        max_value = number
+
+if max_value is not None:
+    print("Max:", max_value)
+else:
+    print("No numbers entered.")
+
